15b5a8b557daf1cc55b8e979a5cac8813f8875ec
Gonie <111123554+GonieAnima@users.noreply.github.com>
2025-01-17 12:49:27 +0100

    added a locatePos function that locates the position of the stepper

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import RPi.GPIO as GPIO
 import time
 from motion.TB6600_interface import Stepper
+from motion.locatePos import posLocate
 
 if __name__ == '__main__':
-    
+    x_stepper = Stepper(11, 13, 200)
 
-GPIO.cleanup() 
+GPIO.cleanup()
---
+++ motion/locatePos.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+from motion.TB6600_interface import Stepper
+
+def posLocate (endX, endY):         # endX and endY are the GPIO pins for the endstops
+    GPIO.setup(endX,GPIO.OUT)
+    GPIO.setup(endY,GPIO.OUT)
+    x_stepper.setSpeed(30)
+    y_stepper.setSpeed(30)
+    if GPIO.input(endX) == 0:
+        x_stepper.step(-1)
+    if GPIO.input(endY) == 0:
+        y_stepper.step(-1)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            GPIO.setmode(GPIO.BOARD)
+            x_stepper = Stepper(11, 13, 200)
+            y_stepper = Stepper(15, 16, 200)
+            posLocate()
+        except KeyboardInterrupt:
+            print("Exiting")
+            break
+        
+    print("Cleaning up")
+    GPIO.cleanup()
