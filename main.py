06899258761544f05c47718784e376f67a4a3da5
Gonie <111123554+GonieAnima@users.noreply.github.com>
2025-01-17 13:01:53 +0100

    updated the main program to run with input (x,y)

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ from motion.TB6600_interface import Stepper
 from motion.locatePos import posLocate
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BOARD)
     x_stepper = Stepper(11, 13, 200)
+    y_stepper = Stepper(15, 16, 200)
+    posLocate(18, 22)
+    x_stepper.setSpeed(200)
+    y_stepper.setSpeed(200)
+    while True:
+        try:
+            input_str = input("Enter positions (x,y): ")
+            x_pos, y_pos = map(int, input_str.split(','))
+            x_stepper.move_to(x_pos)
+            y_stepper.move_to(y_pos)
+        except KeyboardInterrupt:
+            print("Exiting")
+            break
 
-GPIO.cleanup()
+    print("Cleaning up")
+    GPIO.cleanup()
--- motion/locatePos.py
+++ motion/locatePos.py
@@ -1,17 +1,15 @@
 import RPi.GPIO as GPIO
-import time
 from motion.TB6600_interface import Stepper
 
 def posLocate (endX, endY):         # endX and endY are the GPIO pins for the endstops
-    GPIO.setup(endX,GPIO.OUT)
-    GPIO.setup(endY,GPIO.OUT)
+    GPIO.setup(endX,GPIO.IN)
+    GPIO.setup(endY,GPIO.IN)
     x_stepper.setSpeed(30)
     y_stepper.setSpeed(30)
     if GPIO.input(endX) == 0:
         x_stepper.step(-1)
     if GPIO.input(endY) == 0:
         y_stepper.step(-1)
-
 
 if __name__ == '__main__':
     while True:
